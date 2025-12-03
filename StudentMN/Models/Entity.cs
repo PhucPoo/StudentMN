b158a2c253cd789e885f00accbc6502735821499
Admin <Admin@DESKTOP-T45MDSR>
2025-12-03 08:46:46 +0700

    add model entity

---
+++ StudentMN/Models/Entity.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentMN.Models
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
